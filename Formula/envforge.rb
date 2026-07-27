c76dc55eef4f215e26b05416c63aa7e1984a1ba0
emre erinc <emreerinc@emre-macbook-air.taild76785.ts.net>
2026-07-27 21:26:47 +0300

    update checksum

--- Formula/envforge.rb
+++ Formula/envforge.rb
@@ -2,7 +2,7 @@ class Envforge < Formula
   desc "Open-source AI-safe environment manager and secret provider CLI"
   homepage "https://github.com/emreerinc/envforge"
   url "https://github.com/emreerinc/envforge/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  sha256 "42d072694f21850ab4b80c895a3ff5b80dc7055e31c039ee17b72d8fcdd0f94f"
   license "Elastic-2.0"
   head "https://github.com/emreerinc/envforge.git", branch: "main"
 
