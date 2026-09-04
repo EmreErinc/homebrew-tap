9fb9c833733911343049c3be98f8c3beb8081994
emre erinc <emreerinc@emre-MacBook-Air.local>
2026-09-05 00:58:48 +0300

    update version

--- Formula/envforge.rb
+++ Formula/envforge.rb
@@ -1,7 +1,7 @@
 class Envforge < Formula
   desc "AI-safe environment manager and secret provider CLI"
   homepage "https://envforge.tech"
-  url "https://github.com/emreerinc/envforge/archive/refs/tags/v1.0.2.tar.gz"
+  url "https://github.com/emreerinc/envforge/archive/refs/tags/v1.0.3.tar.gz"
   sha256 "dbf88daeac97e7670983a08094d41af38b398d1a89eb38c3b3e560de3d64b4a0"
   license "Elastic-2.0"
   head "https://github.com/emreerinc/envforge.git", branch: "main"
